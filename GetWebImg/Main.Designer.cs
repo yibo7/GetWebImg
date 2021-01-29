c0a68571a3fd1fd8556f65b006ac34e26801a9d5
cqs263 <cqs263@163.com>
2021-01-29 12:37:28 +0800

    项目初始化

---
+++ GetWebImg/Main.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace GetWebImg
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.wbB = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.txtUrl = new System.Windows.Forms.ToolStripTextBox();
+            this.btnTest = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stInfo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wbB
+            // 
+            this.wbB.Location = new System.Drawing.Point(12, 45);
+            this.wbB.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbB.Name = "wbB";
+            this.wbB.Size = new System.Drawing.Size(721, 467);
+            this.wbB.TabIndex = 1;
+            this.wbB.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbB_DocumentCompleted);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtUrl,
+            this.btnTest});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(757, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(500, 25);
+            // 
+            // btnTest
+            // 
+            this.btnTest.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnTest.Image = ((System.Drawing.Image)(resources.GetObject("btnTest.Image")));
+            this.btnTest.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(23, 22);
+            this.btnTest.Text = "测试";
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stInfo});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 538);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(757, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stInfo
+            // 
+            this.stInfo.Name = "stInfo";
+            this.stInfo.Size = new System.Drawing.Size(56, 17);
+            this.stInfo.Text = "等待就绪";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 560);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.wbB);
+            this.Name = "Main";
+            this.Text = "生成网页快照";
+            this.SizeChanged += new System.EventHandler(this.Main_SizeChanged);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.WebBrowser wbB;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox txtUrl;
+        private System.Windows.Forms.ToolStripButton btnTest;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stInfo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ GetWebImg/Main.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EbSite.ApiEntity;
+using XS.Core;
+using XS.Core.WebApiUtils;
+
+namespace GetWebImg
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            wbB.ScrollBarsEnabled = false;
+            wbB.ScriptErrorsSuppressed = true; //设置为True,可禁止弹出脚本错误对话框
+            wbB.Width = m_BrowserWidth;
+            wbB.Height = m_BrowserHeight;
+            //wbB.Navigate("http://www.com");
+            bll = new ApiBll(sAPI, key);
+        }
+
+        private EbSite.ApiEntity.Content model;
+        string key = "3z0Pt7mTIieshFoPvpzGws2YoBZQ8XXASuDyILPmH1Mv8cUsfE+Phb2WNmBHbt4SDwenN9HGOq2BfWVxVWYFKcG9w3uwn2JuZjwkD1ryAyAr3ky4jrSjpqvi7gjl90/ht45LZE2rmBu7e6diU9HeT6szs0wlQn2RiwpuBFswL+2z1rdhbcu7tpVYw/CtM9Kah7tuHC5ZT+kUfiXX28vu3g==";
+        private string sAPI = "http://anli.ebsite.net/api";
+        private ApiBll bll;
+        private bool IsStop = true;
+        private void Goto()
+        {
+
+            ContentQueryRezult rz = GetModel();
+
+            if (!Equals(rz,null)&&rz.Count > 0)
+            {
+                IsStop = false;
+                   model =  rz.Data[0];
+
+                string sUrl = model.Annex3;
+                stInfo.Text = string.Format("正在载入{0}", sUrl);
+                wbB.Navigate(sUrl);
+                 
+            }
+            else
+            {
+                IsStop = true;
+            }
+
+        }
+
+        private ContentQueryRezult GetModel()
+        {
+            EbSite.ApiEntity.QueryModel mdQueryModel = new QueryModel();
+            mdQueryModel.SiteId = 1;
+            mdQueryModel.Fields = "";
+            mdQueryModel.PageSize = 1;
+            mdQueryModel.PageIndex = 1;
+            mdQueryModel.OrderBy = "id ASC";
+
+
+            List<ContentQuery> cqs = new List<ContentQuery>();
+            ContentQuery cq = new ContentQuery();
+            cq.ColumName = "ClassID";
+            cq.ColumValue = "1";//注意，如果是区间查询，这里要填写两个值，用逗号分开
+            cq.QueryType = ContentQueryType.精确;//默认为精确查询
+            cq.LinkType = ContentQueryLinkType.AND;//与下一个字段的链接方式，如果是最后一个字段，可不用填写
+            cqs.Add(cq);
+
+            cq = new ContentQuery();
+            cq.ColumName = "Annex3";
+            cq.ColumValue = "''";
+            cq.QueryType = ContentQueryType.不等于;
+            cqs.Add(cq);
+
+            cq = new ContentQuery();
+            cq.ColumName = "Annex7";
+            cq.QueryType = ContentQueryType.是否为空IsNull;
+            cqs.Add(cq);
+
+
+
+
+            mdQueryModel.Wheres = cqs;//你可以注掉此条件，将查找所有数据
+
+            ContentQueryRezult rz = bll.PostModel<ContentQueryRezult>("queryc/loadlist", mdQueryModel);
+
+            return rz;
+        }
+
+        private int m_BrowserWidth = 1440;
+        private int m_BrowserHeight = 1440;
+        private int imgWidth = 1440;
+        private int imgHeight = 1440;
+
+        private void wbB_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+            stInfo.Text = string.Format("{0}载入完成", model.Annex3);
+
+            Thread st = new Thread(() =>
+            {
+                if (!string.IsNullOrEmpty(model.Annex1)) //url的md5
+                {
+
+                    WebBrowser m_WebBrowser = (WebBrowser)sender;
+                    Thread.Sleep(3000);
+                    Bitmap m_Bitmap = new Bitmap(m_WebBrowser.Bounds.Width, m_WebBrowser.Bounds.Height);
+                    m_WebBrowser.BringToFront();
+                    m_WebBrowser.DrawToBitmap(m_Bitmap, m_WebBrowser.Bounds);
+                    m_Bitmap = (Bitmap)m_Bitmap.GetThumbnailImage(imgWidth, imgHeight, null, IntPtr.Zero);
+                    string sUrl = wbB.Url.ToString();
+                    string sMd5 = model.Annex1;// XS.Core.XsUtils.MD5(sUrl);
+                    string sVPath = string.Format("webimg/{0}.png", sMd5);
+                    string sFileName = string.Format("{0}{1}{2}.png", Application.StartupPath, "\\webimg\\", sMd5);
+                    //System.Drawing.Image image = new System.Drawing.Bitmap(m_Bitmap);
+                    //image.Save(sFileName);
+                    model.Annex7 = sVPath;
+                    try
+                    {
+                        m_Bitmap.Save(sFileName);
+
+                        long KSize = (new System.IO.FileInfo(sFileName)).Length;
+                        float testFloat = 0;
+
+                        if (float.TryParse(Math.Round(KSize / (float)1024, 2).ToString(), out testFloat))
+                        {
+                            model.Annex19 = testFloat;
+                        }
+
+
+                        stInfo.Text = string.Format("{0}快照保存成功", sUrl);
+
+
+
+
+
+
+                    }
+                    catch (Exception exception)
+                    {
+                        model.Annex19 = 0;
+
+                    }
+
+                    ApiMessage<int> rzUpdate = bll.PostModel<ApiMessage<int>>("content", model);
+
+                    stInfo.Text = string.Format("{0}快照保存成功并提交更新", sUrl);
+                    Thread.Sleep(2000);
+                }
+                
+                Goto();
+
+
+            });
+            st.SetApartmentState(ApartmentState.STA);
+            st.Start();
+
+
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1_Tick(sender, e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsStop)
+            {
+                ContentQueryRezult rz = GetModel();
+
+                if (!Equals(rz, null) && rz.Count > 0)
+                {
+                    Goto();
+                }
+               
+            }
+           
+        }
+
+        private void Main_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)  //判断是否最小化
+            {
+                this.ShowInTaskbar = false;  //不显示在系统任务栏
+                notifyIcon.Visible = true;  //托盘图标可见
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.ShowInTaskbar = true;  //显示在系统任务栏
+            this.WindowState = FormWindowState.Normal;  //还原窗体
+            notifyIcon.Visible = false;  //托盘图标隐藏
+
+           
+        }
+    }
+     
+}
